bc0f41ea6e22993c72620ee85cfb53d4f710c0af
Sakthiswari E <elansakthiswari@gmail.com>
2025-11-26 06:10:46 -1200

    Adding notification

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -10,10 +10,10 @@ export default function MainLayout({ children }) {
 
   return (
     <div className="flex min-h-screen w-full bg-[#F0F2F5]">
+      
       {/* Sidebar */}
-
       {!hideSidebar && (
-        <div className="hidden md:block w-60 bg-white border-r border-gray-200">
+        <div className="w-60 bg-white border-r border-gray-200">
           <Sidebar />
         </div>
       )}
@@ -32,43 +32,3 @@ export default function MainLayout({ children }) {
     </div>
   );
 }
-
-
-
-
-
-
-
-
-
-
-// import React from "react";
-// import { useLocation } from "react-router-dom";
-// import Sidebar from "../components/Sidebar";
-
-// export default function MainLayout({ children }) {
-//   const location = useLocation();
-//   const hideSidebarRoutes = ["/login", "/register"];
-//   const hideSidebar = hideSidebarRoutes.includes(location.pathname);
-
-//   return (
-//     <div className="flex min-h-screen w-full bg-[#F0F2F5]">
-
-//       {/* Sidebar (desktop only) */}
-//       {!hideSidebar && (
-//         <div className="hidden md:block w-60 bg-white border-r border-gray-200">
-//           <Sidebar />
-//         </div>
-//       )}
-
-//       {/* Main Content Area */}
-//       <main
-//         className={`flex-1 w-full px-4 md:px-6 pb-6 overflow-y-auto ${
-//           !hideSidebar ? "pt-6" : ""
-//         }`}
-//       >
-//         {children}
-//       </main>
-//     </div>
-//   );
-// }
